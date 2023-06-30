47055dbd6c4997f6a4ac19ccd4860a445777eed1
GrifTheDev <70777013+GrifTheDev@users.noreply.github.com>
2023-06-30 16:13:30 +0200

    extended config so that tailwind's defaults do not get wiped

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    fontFamily: {
-      "mulish-bold": ['Mulish-Bold', 'sans-serif'],
-      "mulish-semibold": ['Mulish-SemiBold', 'sans-serif'],
-      "mulish-regular": ['Mulish-Regular', 'sans-serif'],
-      "mulish-light": ['Mulish-Light', 'sans-serif'],
-      sans: ['Mulish-Regular', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        "mulish-bold": ["Mulish-Bold", "sans-serif"],
+        "mulish-semibold": ["Mulish-SemiBold", "sans-serif"],
+        "mulish-regular": ["Mulish-Regular", "sans-serif"],
+        "mulish-light": ["Mulish-Light", "sans-serif"],
+        sans: ["Mulish-Regular", "sans-serif"],
+      },
+      colors: {
+        href: "#008CDA",
+        inputBorder: "#979797",
+        buttonRed: "#FF5876",
+        buttonShadowRed: "#FEC5CF",
+        pinkish: "#FFC6C6",
+        white: "#FFFFFF",
+      },
+      boxShadow: {
+        "button1-idle": "0px 4px 0px 0px rgba(254, 197, 207, 1)",
+        "button1-hover": "0px 7px 0px 0px rgba(254, 197, 207, 1)",
+        "button1-acitve": "0px 0px 0px 0px rgba(254, 197, 207, 1)",
+      },
     },
-    colors: {
-      "href": "#008CDA",
-      "inputBorder": "#979797",
-      "buttonRed": "#FF5876",
-      "buttonShadowRed": "#FEC5CF",
-      "pinkish": "#FFC6C6",
-      "white": "#FFFFFF",
-    },
-    boxShadow: {
-      "button1-idle": "0px 4px 0px 0px rgba(254, 197, 207, 1)",
-      "button1-hover": "0px 7px 0px 0px rgba(254, 197, 207, 1)",
-      "button1-acitve": "0px 0px 0px 0px rgba(254, 197, 207, 1)"
-    }
   },
   plugins: [],
-}
-
+};
